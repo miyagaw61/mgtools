b88a1f502a1f9f8e81b8154cf5d6f95e75856d9c
root <miyagaw61@gmail.com>
2017-05-02 14:21:35 +0900

    edit_twitter.py

--- twitter.py
+++ twitter.py
@@ -4,10 +4,10 @@ def writefile(buf_arg,file_name):
     with open(file_name, 'wb') as f:
         f.write(buf_arg)
 
-CK = ''
-CS = ''
-AT = ''
-AS = ''
+CK = 'JaBCMafqgxW6g3ismku4NQpFf'
+CS = '2fJu5sU8ESQV08KiPyALFYTXVQkSnQTz9lizTDFnZSHdK8GPlZ'
+AT = '2473741867-QNEei8GMk2YCJMtgS1ZoKNYJbyLuBwpqSRzMTRQ'
+AS = 'gPrnZnKo7dgAnKNmRR64ncjfjqUrYdnLkh5L0x7zPARdR'
 
 auth = tweepy.OAuthHandler(CK, CS)
 auth.set_access_token(AT, AS)
@@ -32,26 +32,18 @@ for status in tl:
     i = 0
     while(i < len(oklist)-1):
         if(len(re.findall(oklist[i].decode('utf_8'), status.text)) > 0):
-#            print("-----------------------------------------------------------------------------------------------------------------------------------------------")
-#            print('[+] okflg on !!!!')
-            #print('[+] status.text: ' + status.text.encode('utf_8'))
-#            print('[+] oklist[i]: ' + oklist[i])
             ngflg = 0
             okflg = 1
         i = i + 1
-#    print('favorite: ' + str(status.favorite_count))
-#    print('retweet: '  + str(status.retweet_count))
-#    print('okflg: '    + str(okflg))
-#    print('ngflg: '    + str(ngflg))
     if(status.favorite_count > 0 or status.retweet_count > 0 or okflg > 0):
         lastid = int(open('/home/miyagaw/Documents/lastid', 'r').read())
-        if(status.id > lastid-40000000000000):
+        if(status.id > lastid):
             if(ngflg == 0):
-                print("-----------------------------------------------------------------------------------------------------------------------------------------------")
                 if(status.in_reply_to_status_id != None):
                     namelist   = []
                     screenlist = []
                     textlist   = []
+                    os.system('echo ' + str(status.id) + ' > ~/Documents/lastid')
                     rep = api.get_status(status.in_reply_to_status_id)
                     namelist.append(rep.user.name)
                     screenlist.append(rep.user.screen_name)
@@ -64,16 +56,21 @@ for status in tl:
                     namelist.reverse()
                     screenlist.reverse()
                     textlist.reverse()
-                    print('[-]' + namelist[0].encode('utf_8') + ' @' + screenlist[0].encode('utf_8'))
+                    print("\n" + "#"*100)
+                    print('### [-]' + namelist[0].encode('utf_8') + ' @' + screenlist[0].encode('utf_8'))
                     print(textlist[0].encode('utf_8'))
                     i = 1
                     while(i < len(namelist)):
-                        print(' | \n' + ' -> [+]' + namelist[i].encode('utf_8') + ' @' +  screenlist[1].encode('utf_8'))
+                        print('### | \n' + '### -> [+]' + namelist[i].encode('utf_8') + ' @' +  screenlist[1].encode('utf_8'))
                         print(textlist[i].encode('utf_8'))
                         i = i + 1
-                    print(' | \n' + ' -> [+]' + status.user.name.encode('utf_8') + ' @' +  status.user.screen_name.encode('utf_8'))
+                    print('### | \n' + '### -> [+]' + status.user.name.encode('utf_8') + ' @' +  status.user.screen_name.encode('utf_8'))
                     print(status.text.encode('utf_8'))
+                    print("#"*100 + '\n')
                 else:
-                    print('[+]' + status.user.name.encode('utf_8') + ' @' +  status.user.screen_name.encode('utf_8'))
+                    os.system('echo ' + str(status.id) + ' > ~/Documents/lastid')
+                    line = len('### [+]' + status.user.name.encode('utf_8') + ' @' +  status.user.screen_name.encode('utf_8'))
+                    print("\n" + "#"*100)
+                    print('### [+]' + status.user.name.encode('utf_8') + ' @' +  status.user.screen_name.encode('utf_8') + ' ' + '#'*(99-line))
+                    print("#"*100)
                     print(status.text.encode('utf_8'))
-os.system('echo ' + str(tl[len(tl)-1].id) + ' > ~/Documents/lastid')
