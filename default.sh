1de2b196aa4a584eed88e592a613fd5eb217e79b
miyagaw61 <bgtm1260295@gn.iwasaki.ac.jp>
2017-06-09 01:00:41 +0900

    edit

--- default.sh
+++ default.sh
@@ -6,11 +6,7 @@ hflg=0
 #fi
 
 aflg=0
-bflg=0
-cflg=0
 varA=""
-varB="hoge"
-varC="fuga"
 for arg in "$@" ;do
 	if test "$arg" = "-h" -o "$arg" = "--help" -o "$arg" = "help" ;then
 		hflg=1
@@ -18,41 +14,16 @@ for arg in "$@" ;do
 	elif test $aflg -eq 1 ;then
 		varA=$arg
 		aflg=0
-	elif test $bflg -eq 1 ;then
-		varB=$arg
-		bflg=0
-	elif test $cflg -eq 1 ;then
-		varC=$arg
-		cflg=0
 
 	elif test "$arg" = "-a" ;then
 		aflg=1
-	elif test "$arg" = "-b" ;then
-		bflg=1
-	elif test "$arg" = "-c" ;then
-		cflg=1
 
-	else
-		echo '[+]'$arg
 	fi
 done
 
 if test $hflg -eq 1 ;then
-	echo "Usage: "$(basename $0)" [-h] -a argA [-b argB] [-c argC] expr...
- -h        show help
- -a [argA] set arg1
- -b [argB] set arg2
- -c [argC] set arg3
- expr      set expr"
+	echo -n "Usage: al2jp [file]
+"
 	exit 0
 fi
-
-if test ! "$varA" ;then
-	echo varA is empty!
-	exit 0
-fi
-
-echo varA is $varA
-echo varB is $varB
-echo varC is $varC
 
