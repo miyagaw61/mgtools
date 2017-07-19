afff697458d961dbc6ded15e3098cfbf071c17d6
root <miyagaw61@gmail.com>
2017-07-19 20:45:27 +0900

    [update]default.sh : 大幅アップデート

--- default.sh
+++ default.sh
@@ -6,24 +6,46 @@ hflg=0
 #fi
 
 aflg=0
-varA=""
-for arg in "$@" ;do
-	if test "$arg" = "-h" -o "$arg" = "--help" -o "$arg" = "help" ;then
-		hflg=1
+dflg=0
+xflg=0
+oflg=0
+lstflg=0
 
-	elif test $aflg -eq 1 ;then
-		varA=$arg
-		aflg=0
+case $1 in
+a);;
+d);;
+x);;
+o);;
+*)lstflg=1;;
+esac
 
-	elif test "$arg" = "-a" ;then
-		aflg=1
-
-	fi
-done
+if test $lstflg -eq 1 ;then
+    lst=$(echo $1 | perl -pe 's@(.)@\1 @g')
+    for var in $lst ;do
+        case $var in
+        d) dflg=1;;
+        x) xflg=1;;
+        o) oflg=1;;
+        a) aflg=1;;
+        h) hflg=1;;
+        esac
+    done
+else
+    for var in $@ ;do
+        case $var in
+        d) dflg=1;;
+        x) xflg=1;;
+        o) oflg=1;;
+        a) aflg=1;;
+        h) hflg=1;;
+        esac
+    done
+fi
 
 if test $hflg -eq 1 ;then
 	echo -n "Usage: al2jp [file]
 "
+    rmtmp
 	exit 0
 fi
 
