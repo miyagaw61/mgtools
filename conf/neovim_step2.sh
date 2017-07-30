d5e53b4405650a68ae7a22debbd55d401b2f9872
miyagaw61 <miyagaw61@gmail.com>
2017-07-30 15:34:07 +0900

    [update] : rm installer.sh

--- conf/neovim_step2.sh
+++ conf/neovim_step2.sh
@@ -1,6 +1,7 @@
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
 chmod +x ./installer.sh
 ./installer.sh $HOME/.deinvim/dein
+rm ./installer.sh
 chown -R $(basename $HOME):$(basename $HOME) $HOME/.deinvim
 chown -R $(basename $HOME):$(basename $HOME) $HOME/.config
 chown -R $(basename $HOME):$(basename $HOME) $HOME/.local
