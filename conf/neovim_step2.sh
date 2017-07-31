b6649808947540bcbe4069b57fd6c2a1709f72b4
miyagaw61 <miyagaw61@gmail.com>
2017-07-31 16:16:52 +0900

    [update] : dein

--- conf/neovim_step2.sh
+++ conf/neovim_step2.sh
@@ -5,3 +5,7 @@ rm ./installer.sh
 chown -R $(basename $HOME):$(basename $HOME) $HOME/.deinvim
 chown -R $(basename $HOME):$(basename $HOME) $HOME/.config
 chown -R $(basename $HOME):$(basename $HOME) $HOME/.local
+cp -a $HOME/mgtools/conf/rc/dein.toml $HOME/.deinvim
+cp -a $HOME/mgtools/conf/rc/dein_lazy.toml $HOME/.deinvim
+cp -a $HOME/mgtools/conf/rc/init.vim $HOME/.config/nvim
+pyenv install 3.5.0
