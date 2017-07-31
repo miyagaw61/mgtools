25c6fb48ff5827f04735a430030be9312baad60d
miyagaw61 <miyagaw61@gmail.com>
2017-07-31 16:16:21 +0900

    [add] : netrc

--- conf/first.sh
+++ conf/first.sh
@@ -1,6 +1,7 @@
 cd $HOME/mgtools
 git config --global user.email "miyagaw61@gmail.com"
 git config --global user.name "miyagaw61"
+cp -a $HOME/mgtools/conf/netrc $HOME/.netrc
 
 cd $HOME/mgtools/conf
 echo "precedence ::ffff:0:0/96  100" >> /etc/gai.conf
@@ -13,11 +14,16 @@ if test ! -e $HOME/.vim/bundle/neosnippet-snippets/neosnippets/ ;then
 fi
 cp -a $HOME/mgtools/conf/rc/neosnippets/* $HOME/.vim/bundle/neosnippet-snippets/neosnippets/ 
 
+vim ./step1_apt.sh
+vim ./step2_curl.sh
+vim ./step3_pip.sh
+vim ./step4_git.sh
+vim ./step5_wget.sh
+vim ./step6_arch.sh
+
 ./step1_apt.sh
 ./step2_curl.sh
 ./step3_pip.sh
 ./step4_git.sh
 ./step5_wget.sh
-
-#64bit only
 ./step6_arch.sh
