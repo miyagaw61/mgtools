d8db9a466db6a1e152902485d48f1c62f75162ab
miyagaw61 <miyagaw61@gmail.com>
2017-08-04 13:36:00 +0900

    [update]conf/first.sh : すべてstepファイルに分割した

--- conf/first.sh
+++ conf/first.sh
@@ -1,19 +1,4 @@
-cd $HOME/mgtools
-git config --global user.email "miyagaw61@gmail.com"
-git config --global user.name "miyagaw61"
-cp -a $HOME/mgtools/conf/netrc $HOME/.netrc
-
-cd $HOME/mgtools/conf
-echo "precedence ::ffff:0:0/96  100" >> /etc/gai.conf
-
-cp -a $HOME/mgtools/conf/rc/bashrc $HOME/.bashrc
-cp -a $HOME/mgtools/conf/rc/vimrc /etc/vim/vimrc
-cp -a $HOME/mgtools/conf/rc/vimrc /usr/share/vim/vimrc
-if test ! -e $HOME/.vim/bundle/neosnippet-snippets/neosnippets/ ;then
-    mkdir -p $HOME/.vim/bundle/neosnippet-snippets/neosnippets/
-fi
-cp -a $HOME/mgtools/conf/rc/neosnippets/* $HOME/.vim/bundle/neosnippet-snippets/neosnippets/ 
-
+vim ./step0_cp.sh
 vim ./step1_apt.sh
 vim ./step2_curl.sh
 vim ./step3_pip.sh
@@ -21,6 +6,7 @@ vim ./step4_git.sh
 vim ./step5_wget.sh
 vim ./step6_arch.sh
 
+./step0_cp.sh
 ./step1_apt.sh
 ./step2_curl.sh
 ./step3_pip.sh
