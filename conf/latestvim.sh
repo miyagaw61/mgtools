dc03101bf6ea04d24dba1bbf7eb4c865f1f0385a
miyagaw61 <miyagaw61@gmail.com>
2017-08-08 04:27:03 +0900

    [bugfix]conf/latestvim.sh :

--- conf/latestvim.sh
+++ conf/latestvim.sh
@@ -2,7 +2,11 @@
 if test ! -e $HOME/latestvim ;then
     mkdir -p $HOME/latestvim
 fi
-git clone https://github.com/vim/vim.git
-cd vim
+if test ! -e $HOME/Downloads/vim ;then
+    rm -rf $HOME/Downloads/vim
+fi
+git clone https://github.com/vim/vim.git $HOME/Downloads/vim
+cd $HOME/Downloads/vim
 git checkout -b 8-0-0886 refs/tags/v8.0.0886
-./configure --enable-multibyte --with-features=huge --disable-selinux --enable-terminal --prefix=/home/miyagaw61/latestvim && make -j4 && make -j4 install
+cd src
+./configure --enable-multibyte --with-features=huge --disable-selinux --enable-terminal --prefix=$HOME/latestvim && make -j4 && make -j4 install
