f60844cfe407ea0bbf30530b4575be1c71b295a6
miyagaw61 <miyagaw61@gmail.com>
2017-08-08 06:22:22 +0900

    [update]conf/latestvim.sh : neocompleteのためにluaの導入フローを追加、その他オプション追加

--- conf/latestvim.sh
+++ conf/latestvim.sh
@@ -1,12 +1,14 @@
 #!/bin/sh
-if test ! -e $HOME/latestvim ;then
-    mkdir -p $HOME/latestvim
+if test -e $HOME/latestvim ;then
+    rm -rf $HOME/latestvim
 fi
-if test ! -e $HOME/Downloads/vim ;then
+mkdir -p $HOME/latestvim
+if test -e $HOME/Downloads/vim ;then
     rm -rf $HOME/Downloads/vim
 fi
 git clone https://github.com/vim/vim.git $HOME/Downloads/vim
 cd $HOME/Downloads/vim
 git checkout -b 8-0-0886 refs/tags/v8.0.0886
 cd src
-./configure --enable-multibyte --with-features=huge --disable-selinux --enable-terminal --prefix=$HOME/latestvim && make -j4 && make -j4 install
+apt-get -y install lua5.1 luajit liblua5.1-0 liblua5.1-0-dev ruby2.3 ncurses-dev libluajit-5.1-dev
+./configure --enable-multibyte --with-features=huge --disable-selinux --enable-terminal --enable-perlinterp=dynamic --enable-rubyinterp=dynamic --with-ruby-command=/usr/bin/ruby2.3 --enable-pythoninterp=dynamic --enable-luainterp=dynamic --with-luajit --enable-gpm --enable-cscope --enable-fontset --prefix=$HOME/latestvim && make -j4 && make -j4 install
