2d82a3dbc1d5b3e5c5f3ad6d7b201477000f8b6c
miyagaw61 <miyagaw61@gmail.com>
2017-10-25 20:15:35 +0900

    [add]neovim_step1.sh : apt install texinfo

--- conf/neovim_step1.sh
+++ conf/neovim_step1.sh
@@ -1,5 +1,5 @@
 yes | pip3 install neovim
-apt-get -y install git libtool autoconf automake cmake g++ pkg-config unzip  
+apt -y install git libtool autoconf automake cmake g++ pkg-config unzip texinfo
 cd ~/
 git clone https://github.com/neovim/neovim.git
 cd neovim
