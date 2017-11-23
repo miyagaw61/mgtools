1efb70db84df99dfc4d1209b05b2cb8c38e3d878
miyagaw61 <miyagaw61@gmail.com>
2017-11-23 12:50:29 +0900

    [update]neovim_step{1,2}.sh :

--- conf/neovim_step1.sh
+++ conf/neovim_step1.sh
@@ -1,9 +1,10 @@
 set -eu
+bin_path="$HOME/bin"
+repos_path="$HOME/repos"
 yes | pip install neovim
 apt -y install git libtool autoconf automake cmake g++ pkg-config unzip texinfo
-cd $HOME
-git clone https://github.com/neovim/neovim.git $HOME/repos/neovim
-cd $HOME/repos/neovim
-yes | make CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$HOME/bin/neovim"
+git clone https://github.com/neovim/neovim.git $repos_path/neovim
+cd $repos_path/neovim
+yes | make CMAKE_EXTRA_FLAGS="-DCMAKE_INSTALL_PREFIX=$bin_path/neovim"
 yes | make install
 mkdir -p ~/.config/nvim
--- conf/neovim_step2.sh
+++ conf/neovim_step2.sh
@@ -1,12 +1,12 @@
 set -eu
 curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
 chmod +x ./installer.sh
-./installer.sh $HOME/.deinvim/dein
+./installer.sh $HOME/.cache/dein
 rm ./installer.sh
-chown -R $(basename $HOME):$(basename $HOME) $HOME/.deinvim
+chown -R $(basename $HOME):$(basename $HOME) $HOME/.cache
 chown -R $(basename $HOME):$(basename $HOME) $HOME/.config
 chown -R $(basename $HOME):$(basename $HOME) $HOME/.local
 chown -R $(basename $HOME):$(basename $HOME) $HOME/.vim
-cp -a $MGTOOLS_ROOT/conf/rc/dein.toml $HOME/.deinvim
-cp -a $MGTOOLS_ROOT/conf/rc/dein_lazy.toml $HOME/.deinvim
-cp -a $MGTOOLS_ROOT/conf/rc/init.vim $HOME/.config/nvim
+cp -a $MGTOOLS_ROOT/conf/rc/vim/plugins/dein.toml $HOME/.cache/dein/
+cp -a $MGTOOLS_ROOT/conf/rc/vim/plugins/dein_lazy.toml $HOME/.cache/dein/
+cp -a $MGTOOLS_ROOT/conf/rc/vim/plugins/init.vim $HOME/.config/nvim/
