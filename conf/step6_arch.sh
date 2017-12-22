9dab52fa102bf46e8305714673f92e33289d6878
miyagaw61 <miyagaw61@gmail.com>
2017-12-22 16:48:35 +0900

    [update]

--- conf/step6_arch.sh
+++ conf/step6_arch.sh
@@ -1,5 +1,8 @@
 #64bit_only
+set -eu
+apt -y update
+apt -y upgrade
 dpkg --add-architecture i386
-apt-get -y update
-apt-get -y install libc6:i386 libncurses5:i386 libstdc++6:i386
-apt-get -y install gcc-multilib g++-multilib
+apt -y update
+apt -y install libc6:i386 libncurses5:i386 libstdc++6:i386
+apt -y install gcc-multilib g++-multilib
